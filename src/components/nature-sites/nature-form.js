95a7a1ee17a12c9857867d4b7c750e202718819a
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-24 12:13:37 -0700

    Created basic skeletoskeleton for nature-form.js

--- src/components/nature-sites/nature-form.js
+++ src/components/nature-sites/nature-form.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export class natureForm extends React.Component {
-  
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <p>Hello world</p>
+  }
 }
