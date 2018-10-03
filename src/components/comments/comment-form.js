30f11ba4e241b24069e01296115ff401cfc3b0a9
rpinto <rachel.lea.pinto@gmail.com>
2018-10-03 12:19:48 -0700

    debugging comment moddal

--- src/components/comments/comment-form.js
+++ src/components/comments/comment-form.js
@@ -3,19 +3,25 @@ import CommentList from './comment-list';
 import CommentModal from './comment-modal';
 
 class Comment extends React.Component {
+  state = { show: false }
+
+  showModal = () => {
+    this.setState({ show: true });
+  }
+  
+  hideModal = () => {
+    this.setState({ show: false });
+  }
 
   render() {
     return (
       <div className="comment-header-container">
         //Add a route for comments
         //Add a route for reviews on comments so link on Line 10 will be functional
-        <h1>Have you been here? Write a</h1>
-        <CommentModal show={this.state.show} handleClose={this.hideModal}>
-          <p>Modal to write comments form goes here</p>
-        </CommentModal>
-        <button type="button" onClick={this.showModal}>
-          write a review
-        </button>
+        <h1>Have you been here?</h1>
+        <button type='button' onClick={this.showModal}>Write a review</button>
+
+        <CommentModal  show={this.state.show} handleClose={this.hideModal}></CommentModal>
 
         <p>Check out what people are saying!</p>
         <CommentList />
--- src/components/comments/comment-modal.js
+++ src/components/comments/comment-modal.js
@@ -1,47 +1,46 @@
 import React from "react";
 
 class CommentModal extends React.Component {
-
+    
   render() {
-    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
-
+      const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+  
     return (
+      <main>
       <div className={showHideClassName}>
-        {/* When we have time for styling - switch to Star ratings with fontAwesome
-        But for now...we'll use regular radio buttons */}
-        <form>
-          <p>Share your thoughts!</p>
-          <div>
-            <input type="radio" id="starRating1"
-              name="star" value="1" />
-            <label for="starRating1">1</label>
+            <form className ='modal-main'>
+        <p>Share your thoughts!</p>
+        <div>
+          <input type="radio" id="starRating1"
+            name="star" value="1" />
+          <label for="starRating1">1</label>
 
-            <input type="radio" id="starRating2"
-              name="star" value="2" />
-            <label for="starRating2">2</label>
+          <input type="radio" id="starRating2"
+            name="star" value="2" />
+          <label for="starRating2">2</label>
 
-            <input type="radio" id="starRating3"
-              name="star" value="3" />
-            <label for="starRating3">3</label>
+          <input type="radio" id="starRating3"
+            name="star" value="3" />
+          <label for="starRating3">3</label>
 
-            <input type="radio" id="starRating4"
-              name="star" value="4" />
-            <label for="starRating4">4</label>
+          <input type="radio" id="starRating4"
+            name="star" value="4" />
+          <label for="starRating4">4</label>
 
-            <input type="radio" id="starRating5"
-              name="star" value="5" />
-            <label for="starRating5">5</label>
-          </div>
-          <input type="text" id="commentSubject" placeholder="subject line"></input>
-          <textarea name="comment-text-area" rows="3" columns="33" placeholder="Write a comment!"></textarea>
-          <div>
-            <button type="submit" onClick={handleClose}>Submit</button>
-          </div>
-        </form>
-      </div>
-    );
+          <input type="radio" id="starRating5"
+            name="star" value="5" />
+          <label for="starRating5">5</label>
+        </div>
+        <input type="text" id="commentSubject" placeholder="subject line"></input>
+        <textarea name="comment-text-area" rows="3" columns="33" placeholder="Write a comment!"></textarea>
+        <div>
+          <button type="submit" onClick={handleClose}>Submit</button>
+        </div>
+      </form>
 
-  }
-}
+    </div>
+    </main>
+  );
+};
 
 export default CommentModal;
