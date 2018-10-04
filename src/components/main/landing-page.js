f6743f36707fd90213f89824cdb98c137a8fb8d9
rpinto <rachel.lea.pinto@gmail.com>
2018-10-04 08:21:10 -0700

    adding close functionality on to write a comment button

--- src/components/main/landing-page.js
+++ src/components/main/landing-page.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 
-import LoginForm from "../users/login-form";
+import "./styles/landing-page.css"
 
 export function LandingPage(props) {
   // If we are logged in redirect straight to the user's dashboard
@@ -12,9 +12,18 @@ export function LandingPage(props) {
 
   return (
     <div className="home">
-      <h2>Parks!</h2>
-      <LoginForm />
-      <Link to="/register">Create Account</Link>
+      <h1>Get Outside</h1>
+      <main className="landing-img-section">
+      <img src={require('../../images/landing-img.jpg')} alt="Image of a park in Honolulu, Hawaii" className="landing-img" />
+      <div>
+        <h2>Start Here</h2>
+        <p>What's in your neighborhood?</p>
+      </div>
+      </main>
+  <div>
+      <Link to="/register">Sign Up</Link>
+      <Link to="/login">Log In</Link>
+      </div>
     </div>
   );
 }
