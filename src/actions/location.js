fd2727a1b7dc5bf083df96d1c6aee00e133cf658
Cecille Salazar <cecillersalazar@gmail.com>
2018-10-04 12:59:59 -0700

    Switching from axios to fetch - might break. About to do install for fetch which is not being recognized

--- src/actions/location.js
+++ src/actions/location.js
@@ -148,32 +148,27 @@ export const geocodeSuccess = (latlng) => ({
 })
 
 export const geocode = locationObject => dispatch => {
-  //console.log('This is the locationObject as it enters geocode function!', locationObject.currentLocation)
-  var location = {
+  console.log("Request to geocode initiated", locationObject)
+  const token = loadAuthToken();
+  let locationMapObject = {
     adminDistrict: locationObject.currentLocation.state,
     postalCode: locationObject.currentLocation.zipCode,
     locality: locationObject.currentLocation.city,
     addressLine: locationObject.currentLocation.address,
-    key: 'Av25hukp36GNb2z84eLlEqTM89_Ac6TK04lIvT6yraWADNTKr8YJFQ1DpR3Dac6g'
   }
-  //console.log("This is the location object after it's been pieced apart in geocode function!", location)
-
-  let latitude;
-  let longitude;
-  let coordinates;
-
-  axios.get(`http://dev.virtualearth.net/REST/v1/Locations/US/${location.adminDistrict}/${location.postalCode}/${location.locality}/${location.addressLine}?o=json&key=${location.key}`)
+  console.log('This is the object before it goes to fetch!', locationMapObject)
+  fetch(`${BACKEND_URL}/locationmap/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(locationMapObject)
+  })
   .then(res => {
-    console.log("Latitude within axios", res.data.resourceSets[0].resources[0].point.coordinates[0])
-    console.log("Longitude within axios", res.data.resourceSets[0].resources[0].point.coordinates[1])
-    latitude = res.data.resourceSets[0].resources[0].point.coordinates[0];
-    longitude = res.data.resourceSets[0].resources[0].point.coordinates[1];
-    coordinates = {
-      lat: latitude,
-      lng: longitude
-    }
-    console.log("Right before coordinates return statement in axios", coordinates);
-    return coordinates;
+    console.log("obtained response", res);
+    normalizeResponseErrors(res);
+    return res.json();
   })
   .then(latlng => {
     console.log("Right before dispatching geocodeSuccess function", latlng);
