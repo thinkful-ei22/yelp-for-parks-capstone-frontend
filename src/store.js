70b38add9dc5d035f388cb3b76cb16b6093d7e98
MitchGianoni <mitchgianoni@yahoo.com>
2018-10-08 12:36:43 -0400

    fixing store to remove root reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/root-reducer";
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import locationReducer from './reducers/location';
+// import commentReducer from "./reducers/comment";
+import loginReducer from './reducers/login';
+import registrationReducer from './reducers/registration';
+import authorReducer from './reducers/author';
+import userReducer from './reducers/user';
 
 const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  combineReducers({
+    user: loginReducer,
+    registration: registrationReducer,
+    location: locationReducer,
+    userProfile: userReducer,
+    authorProfile: authorReducer,
+  }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
 );
 
