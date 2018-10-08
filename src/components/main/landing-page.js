3e5ecdf43ec8968595a8878d4cf44558d7bdce7f
Cecille Salazar <cecillersalazar@gmail.com>
2018-10-08 12:28:03 -0700

    Basic placement achieved in login page

--- src/components/main/landing-page.js
+++ src/components/main/landing-page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
+import './styles/landing-page.css';
 
 import LoginForm from "../users/login-form";
 
@@ -11,10 +12,30 @@ export function LandingPage(props) {
   }
 
   return (
-    <div className="home">
-      <h2>Parks!</h2>
-      <LoginForm />
-      <Link to="/register">Create Account</Link>
+    <div>
+      <div className="intro-text">
+        <h1>GO helps you explore the great outdoors - <br/>
+          right in your own neighborhood</h1>
+
+          <h5>Recommend your favorite nature sites: hiking <br/>
+          trails, parks, ponds, mountains, stargazing, <br/>
+          campgrounds...the sky is the limit!</h5>
+
+          <img className="backpack" src="https://i.postimg.cc/ZnFx9sN4/climbingpack.png" />
+
+          <h5>See what places other users cannot<br/>
+          talking about</h5>
+
+          <img className="tent" src="https://i.postimg.cc/mDSPCvmX/kisspng-camping-tent-campsite-campfire-clip-art-sick-dog-cartoon.png" />
+
+          <h4>Join the discussion.<br/>
+          Make community.<br/>
+          Get Outside.</h4>
+      </div>
+      <div className="login-form-placement">
+        <LoginForm />
+        <Link to="/register">Create Account</Link>
+      </div>
     </div>
   );
 }
