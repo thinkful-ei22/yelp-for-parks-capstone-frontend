690c4112f2dddc0ab57802b044546a9dc023b884
MitchGianoni <mitchgianoni@yahoo.com>
2018-10-11 12:04:00 -0400

    header bar works

--- src/components/main/header-bar.js
+++ src/components/main/header-bar.js
@@ -1,48 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
-//import { clearAuth } from '../actions/auth';
-//import { clearAuthToken } from '../utils/local-storage';
+import { logout } from '../../actions/login';
 import { Link } from 'react-router-dom';
 import './styles/header-bar.css';
 
 export class HeaderBar extends React.Component {
-  logOut() {
-    //this.props.dispatch(clearAuth());
-    //clearAuthToken();
-  }
 
   render() {
-    let dashboard, myProfile;
-    // if (this.props.validUser) {
-    //   dashboard = (
-    //     <Link to="/dashboard">
-    //       <button type="button" name="back-to-dashboard">
-    //         Dashboard{''}
-    //       </button>
-    //     </Link>
-    //   );
-    //   const userId = this.props.loggedIn.currentUser.id;
-    //   myProfile = (
-    //     <Link to={`/profile/${userId}`}>
-    //       <button type="button" name="my-profile">
-    //         My Profile{''}
-    //       </button>
-    //     </Link>
-    //   );
-    // }
+    let dashboard, myProfile, userLogout;
+    if (this.props.validUser) {
+      dashboard = (
+        <Link to="/dashboard">
+          <button type="button" name="back-to-dashboard">
+            Dashboard{''}
+          </button>
+        </Link>
+      );
+      const userId = this.props.loggedIn.currentUser.id;
+      myProfile = (
+        <Link to={`/profile/${userId}`}>
+          <button type="button" name="my-profile">
+            My Profile{''}
+          </button>
+        </Link>
+      );
+      userLogout = (
+        <Link to={'/'}>
+          <button type="button" onClick={() => this.props.dispatch(logout())}>
+            Log Out
+          </button>
+        </Link>
+      );
+    }
 
     return (
       <div className="header-bar">
         <h1>Parks!</h1>
         {dashboard}
         {myProfile}
+        {userLogout}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  validUser: state.user !== null,
+  validUser: state.user.currentUser !== null,
   loggedIn: state.user
 });
 
