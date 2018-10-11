3e407580b80dacfb69c49aec0d797fa98fc8288c
Cecille Salazar <cecillersalazar@gmail.com>
2018-10-11 05:45:53 -0700

    Made some tweaks to 1000px query and finalized 800px query

--- src/components/users/registration-form.js
+++ src/components/users/registration-form.js
@@ -36,7 +36,7 @@ class RegistrationForm extends React.Component {
     return (
       <div className="registration-container">
         <br/>
-        <h1>Sign Up </h1>
+        <h1 className="registration-form-header">Sign Up </h1>
         <form>
           <label htmlFor="registration-username" className="registration-label" />
           <input
