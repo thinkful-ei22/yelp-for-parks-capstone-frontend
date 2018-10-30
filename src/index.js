ec64eba978417de22e957c831b2d21a320fc2e09
MitchGianoni <mitchgianoni@yahoo.com>
2018-10-30 06:35:45 -0400

    added favicon

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import Favicon from 'react-favicon';
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import store from "./store";
 
 ReactDOM.render(
+  <div>
+  <Favicon url="https://s3.us-east-2.amazonaws.com/mg-icons/yfpfavicon.png" />
   <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>,
+  </Provider>
+  </div>,
   document.getElementById("root")
 );
 registerServiceWorker();
